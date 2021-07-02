aa3224be8474f0970a6433c82127a4c89cd9db7d
Liam Percy <liampercy123@gmail.com>
2021-07-02 21:56:07 +1000

    fix: android view modules

--- android/app/src/main/java/com/habitual/MainApplication.java
+++ android/app/src/main/java/com/habitual/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.picker.RNCPickerPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNCPickerPackage());
           return packages;
         }
 
